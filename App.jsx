231a4f7e9a78209c1b01b8a44c01f3682c241b8d
Peter Ferguson <peterferguson95@gmail.com>
2023-11-10 10:18:14 +0000

    chore: insert timestamp crashes app

--- App.jsx
+++ App.jsx
@@ -2,18 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ScrollView, View } from 'react-native';
 import { useEffect, useState } from 'react'
 import { openDatabaseSync, SQLiteProvider, useSQLiteContext, addDatabaseChangeListener } from 'expo-sqlite/next';
+import React from 'react';
+import { Pressable } from 'react-native';
 
 const db = openDatabaseSync( ':memory:', {
   enableChangeListener: true
 } );
 
-console.log( "db", db )
-
 db.execSync(
   'CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER)'
 );
 
-const insertRow = () => db.runSync( 'INSERT INTO test (value, intValue) VALUES (?, ?)', 'test', 123 )
+const insertRow = ( isTimestamp = false ) => db.runSync( 'INSERT INTO test (value, intValue) VALUES (?, ?)', 'test', isTimestamp ? Date.now() : 123 )
 const truncate = () => db.runSync( 'DELETE FROM test' )
 
 console.log( 'insert - ', insertRow() );
@@ -37,6 +37,7 @@ export default function App() {
 const Child = () => {
   const database = useSQLiteContext()
 
+  const [isTimestamp, setIsTimestamp] = useState( false )
   const [rows, setRows] = useState( database.allSync( 'SELECT * FROM test' ) )
 
   const refreshRows = () => setRows( database.allSync( 'SELECT * FROM test' ) )
@@ -53,13 +54,13 @@ const Child = () => {
   }, [] )
 
   useEffect( () => {
-    const interval = setInterval( insertRow, 1000 )
+    const interval = setInterval( () => insertRow( isTimestamp ), 1000 )
 
     return () => {
       interval && clearInterval( interval )
     }
 
-  }, [] )
+  }, [isTimestamp] )
 
   useEffect( () => {
     if ( rows.length > 25 ) truncate()
@@ -68,16 +69,27 @@ const Child = () => {
 
 
   if ( !rows ) return <Text style={ styles.text }>Nothing</Text>
-  return <ScrollView>
-    { rows?.map( ( row, i ) => {
-      return <Text key={ `row-${i}` } style={ styles.text }>{ i } - { row.value }</Text>
-    } ) }
-  </ScrollView>
+  return <View style={ styles.container }>
+    <Pressable
+      style={ { marginVertical: 40 } }
+      onPress={ () => {
+        setIsTimestamp( true )
+      } }>
+      <Text>Insert { isTimestamp ? 'Integer' : 'Timestamp' }</Text>
+    </Pressable>
+    <ScrollView>
+      { rows?.map( ( row, i ) => {
+        return <Text key={ `row-${i}` } style={ styles.text }>{ i } - { row.value }</Text>
+      } ) }
+    </ScrollView>
+
+  </View>
 }
 
 const styles = StyleSheet.create( {
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: '#939393',
     alignItems: 'center',
     justifyContent: 'center',
